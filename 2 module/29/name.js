008e9fb6a4c69c95a4c652301a73b6cc2c571784
zerotimeszero <you@example.com>
2023-12-26 10:25:02 +0000

    fix 29

--- 2 module/29/name.js
+++ 2 module/29/name.js
@@ -5,7 +5,12 @@ const yearOfBirth = '1881'
 const fieldOfStudy = 'Defence Against the Dark Arts'
 
 const method1 = "First name: " + firstName + '\n' + "Last name: " + lastName + '\n' + "Middle name: " + middleName + "\n" + "Year of birth: " + yearOfBirth + "\n" + "Field of study: " + fieldOfStudy;
-const method2 = `First name: ${firstName}\nLast name: ${lastName}\nMiddle name: ${middleName}\nYear of birth: ${yearOfBirth}\nField of study: ${fieldOfStudy}`;
+
+const method2 = `First name: ${firstName}
+Last name: ${lastName}
+Middle name: ${middleName}
+Year of birth: ${yearOfBirth}
+Field of study: ${fieldOfStudy}`;
 
 console.log(method1);
 console.log(method2);
